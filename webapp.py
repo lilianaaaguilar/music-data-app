78ba7835e07e288517216e31a92662d331b4a162
lilianaaaguilar <lilianaguilar2179@icloud.com>
2025-10-22 13:55:38 -0700

    Feat: Add databygenre htmls + Adding to py file

--- webapp.py
+++ webapp.py
@@ -1,1 +1,12 @@
+from flask import Flask
+
 import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def render_about():
+    return render_template('about.html')
+    
+@app.route('/databygenre')
+def render_databygenre():
